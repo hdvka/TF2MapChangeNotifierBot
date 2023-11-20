ff59b4ccafb0f55b7db0fd330a2e0f4bca5c35a2
hdvka <hedvikamusic@gmail.com>
2023-11-20 19:23:37 +0000

    fixed some async issues to get user check working

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ import os
 import a2s
 import asyncio
 import discord
+import logging
 from dotenv import load_dotenv
 from discord.ext import commands
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -23,12 +26,14 @@ last_map = "fake_map"  # Global variable to store the last known map
 async def check_map_periodically(address):
     global last_map
     while True:
-        current_map = get_map(address)
-        if current_map != last_map and not am_i_playing(address):
-            channel = discord.utils.get(bot.get_all_channels(), name='general')  # Replace 'general' with your channel name
-            if channel:
-                await channel.send(f"The map has changed to: {current_map}")
-            last_map = current_map
+        current_map = await get_map(address)
+        if current_map != last_map:
+            playing = await am_i_playing(address)
+            if not playing:
+                channel = discord.utils.get(bot.get_all_channels(), name='general')
+                if channel:
+                    await channel.send(f"The map has changed to: {current_map}")
+                last_map = current_map
         await asyncio.sleep(30)  # Wait for 30 seconds before checking again
 
 @bot.event
@@ -36,15 +41,33 @@ async def on_ready():
     bot.loop.create_task(check_map_periodically(SERVER_ADDRESS))
 
 @bot.command(name='current_map', help='Current map of Uncletopia Chicago 1.')
-async def current_map(ctx):
-    await ctx.send(get_map(SERVER_ADDRESS))
+async def current_map_handler(ctx):
+    await ctx.send(await get_map(SERVER_ADDRESS))
 
-def get_map(address):
-    info = a2s.info(address)
+@bot.command(name='am_i_playing', help='Am I on the currently monitored server?.')
+async def am_i_playing_handler(ctx):
+    await ctx.send(await am_i_playing(SERVER_ADDRESS))
+
+""" @bot.command(name='what_am_i_playing', help='What am I playing according to Discord?')
+async def activity(ctx, user: discord.Member):
+    try:
+        for activity in user.activities:
+            if activity.type == discord.ActivityType.playing:
+                await ctx.send(f"{user.name} is playing {activity.name}")
+                return
+        await ctx.send(f"{user.name} is not playing anything.")
+    except discord.NotFound:
+        await ctx.send("User not found.") """
+
+async def get_map(address):
+    info = await a2s.ainfo(address)
     return info.map_name
 
-def am_i_playing(address):
-    players = a2s.players(address)
-    return any(player.name == STEAM_NAME for player in players)
+async def am_i_playing(address):
+    try:
+        players = await a2s.aplayers(address=address)
+        return any(player.name == STEAM_NAME for player in players)
+    except:
+        return False
 
 bot.run(TOKEN)
